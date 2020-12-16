789192e0a31fa11defb6bc7912e52c728578bc0e
Neil Agarwal <neil@neilagarwal.me>
2020-12-17 02:54:55 +0530

    chore: eslint/prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
+	singleQuote: true,
+	trailingComma: 'all',
+	tabWidth: 4,
+	useTabs: true,
+	jsxSingleQuote: true,
 };
